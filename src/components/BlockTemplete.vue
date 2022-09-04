dfae6028ac1410045da5ed252522e7247e7ed6a5
syoe <shoe657sunflower@yahoo.co.jp>
2022-09-04 10:12:38 +0900

    ブロックテンプレート

---
+++ src/components/BlockTemplete.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="icebox">
+    <a href="https://hicbc.com/tv/ronsetsu/article/?id=post-2295">
+      <img
+        src="https://hicbc.com/tv/ronsetsu/assets/images/post-2295/16352103171kdcv-large.jpg"
+      />
+    </a>
+  </section>
+</template>
+
+<style>
+.icebox a {
+  display: flex;
+}
+</style>
