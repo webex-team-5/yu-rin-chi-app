e80bef5b94d0005e46b037457b45d6fc47e4fcb1
takumi <zekus1018@gmail.com>
2022-09-04 11:04:20 +0900

    アイスを追加

--- src/components/SearchComponet.vue
+++ src/components/SearchComponet.vue
@@ -22,6 +22,7 @@
     <div class="ice" v-for="(item, index) in displayItems" v-bind:key="index">
       <img v-bind:src="item.imgUrl" />
       <ul id="tag">
+        <li>名称：{{ item.name }}</li>
         <li>場所：{{ item.place }}</li>
         <li>味：{{ item.taste }}</li>
         <li>料金：￥{{ item.fee }}</li>
@@ -36,30 +37,74 @@ export default {
     return {
       items: [
         {
-          place: "福岡",
-          taste: "いちご",
-          fee: 120,
+          name: "金太郎ソフト",
+          place: "神奈川",
+          taste: "ミルク",
+          fee: 390,
           imgUrl:
-            "https://kabekin.com/uploads/converted/15/06/26/1262326841-ice-cream-with-sprinkles-30083-kRqx-1280x1024-MM-100.jpg",
+            "https://msp.c.yimg.jp/images/v2/FUTi93tXq405grZVGgDqG-8VOhlzbahf5Xy3y9OeNNbjdk8BIPbedEKjZst8QucAjryTedk9l9FZS7Oa3d35-z9_ibyzwj_qyM-yBAdIKBPAd5T9RokSYv5ZZ_mkxt61f_cEq9tEglz9ZRUKQrCS1PwSODmBi4vgdyJO81676WiCiLg4q6lcq-ryAZ5QF6Fk8yZ_RByd876E0CgTfbH7lCWEXGov2tueIn5PyUQhczA=/sweets06.jpg?errorImage=false",
         },
         {
+          name: "アイスコルネット",
           place: "北海道",
-          taste: "チョコ",
-          fee: 310,
+          taste: "色々",
+          fee: 450,
           imgUrl:
-            "https://th.bing.com/th/id/OIP.7vdQirHbF_f2_JV0b9LP-QHaEo?pid=ImgDet&rs=1",
+            "https://msp.c.yimg.jp/images/v2/FUTi93tXq405grZVGgDqG2ZbYGCyLpR5USnqNmgnrubG2eS9ra12f_cClqxFaU7s0YpLllGAFSC5Tar-YRXTakOkpVqH7Tbh03tHmdQUhODS649_HeUnFW4QrVB7dAxo09XdCAzRB4g1dlWeKmsM_iVMNSL8zhbeiMu3ss173ib8668a00Xma_Sea4JcVNoCO36uwOB9yUqd1JQrwQ-6WVbhp9CX0XeKBMNKc2-ISbM95ycc4ide7JRaRx1e3EnnWtuD6pgYCQ4X0UI7q88nS73MtYp6nOvpxBr4zipLaMrWs6MTQBXVqEK-ddpaa858GVAVuUeHJOIOHEAe7sw6mOjWaBe0whOsFgXSvD1i-V20yUcxQYA1at8goeuOLshyLA-hM4miunjYzddAQkaIvIzoTqhopsqv4IVURj06tnBOjqSbMYAV_BjtnTrSFAZu9KaZUG8v3_Vd5vDUuI32wtbhL-0Ez-nbAwLOYnpymCeb9dq0EgNRbrlfo12Bun9gRK6NLRtKo_zpNiVy2NiIh-qJEeqlR-uDpU8KWnNCa2PheHpE8Qr3vGeUpur_nutveshrBANwW9qjeSn7gD-lqTIA3cIqqsIpqSgaaPFQTZys1lCt-gM44FD_qlnbeDYciBqSRV0cqb9meaeqGYfSNVmbYmLXmClT52jeROrjNx93tVjgG8PJRlAsi47mX3uH/E382A2E382A4E382B9E382B3E383ABE3838DE38383E38388.jpg?errorImage=false",
         },
         {
-          place: "愛知",
-          taste: "チョコ",
-          fee: 420,
+          name: "安曇野りんごソフトクリーム",
+          place: "長野",
+          taste: "りんご",
+          fee: 330,
           imgUrl:
             "https://static.retrip.jp/spot/0a91c428-182c-42a6-845e-c9fce5088d94/images/7f2c2895-f982-40aa-8b56-df546e53820d_l.jpg",
         },
         {
+          name: "本わさびソフト",
+          place: "山梨",
+          taste: "わさび",
+          fee: 350,
+          imgUrl:
+            "https://th.bing.com/th/id/OIP.RsJMVqXOMupjlY0VE7nJvwHaFj?w=220&h=180&c=7&r=0&o=5&dpr=2&pid=1.7",
+        },
+        {
+          name: "ブルーシールアイス",
           place: "沖縄",
-          taste: "チョコ",
-          fee: 1000,
+          taste: "色々",
+          fee: 350,
+          imgUrl:
+            "https://th.bing.com/th/id/OIP.RsJMVqXOMupjlY0VE7nJvwHaFj?w=220&h=180&c=7&r=0&o=5&dpr=2&pid=1.7",
+        },
+        {
+          name: "山江村産　栗ジャムソフト",
+          place: "熊本",
+          taste: "栗",
+          fee: 410,
+          imgUrl:
+            "https://th.bing.com/th/id/OIP.RsJMVqXOMupjlY0VE7nJvwHaFj?w=220&h=180&c=7&r=0&o=5&dpr=2&pid=1.7",
+        },
+        {
+          name: "金箔ソフト",
+          place: "石川",
+          taste: "色々",
+          fee: 891,
+          imgUrl:
+            "https://th.bing.com/th/id/OIP.RsJMVqXOMupjlY0VE7nJvwHaFj?w=220&h=180&c=7&r=0&o=5&dpr=2&pid=1.7",
+        },
+        {
+          name: "ラベンダーソフト",
+          place: "北海道",
+          taste: "ラベンダー",
+          fee: 300,
+          imgUrl:
+            "https://th.bing.com/th/id/OIP.RsJMVqXOMupjlY0VE7nJvwHaFj?w=220&h=180&c=7&r=0&o=5&dpr=2&pid=1.7",
+        },
+        {
+          name: "味道楽ソフトクリーム",
+          place: "秋田",
+          taste: "色々",
+          fee: 330,
           imgUrl:
             "https://th.bing.com/th/id/OIP.RsJMVqXOMupjlY0VE7nJvwHaFj?w=220&h=180&c=7&r=0&o=5&dpr=2&pid=1.7",
         },
@@ -102,6 +147,7 @@ img {
   width: 100%;
   display: flex;
   justify-content: space-around;
+  flex-wrap: wrap;
 }
 .ice {
   display: flex;
