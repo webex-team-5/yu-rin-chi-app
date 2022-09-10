111d93e6d8fde1c0f10ab88f08bb9b5a1f3e0355
yui-ths <takahashiyui.0831@gmail.com>
2022-09-10 11:40:24 +0900

    ラジオボタンを変更しました

--- src/views/IceChart.vue
+++ src/views/IceChart.vue
@@ -2,28 +2,52 @@
   <h1>アイスクリームチャート</h1>
   <form id="Q">
     <p>あなたの食べたい味は？？</p>
-    <input type="radio" name="q1" value="3" v-model="picked" />甘～い
-    <input type="radio" name="q1" value="2" v-model="picked" />すっきり
-    <input type="radio" name="q1" value="1" v-model="picked" />濃厚
+    <input
+      type="radio"
+      name="q1"
+      value="3"
+      v-model="picked"
+      class="Radio-Input"
+    />
+    <span class="Radio-Text">甘～い</span>
+    <input
+      type="radio"
+      name="q1"
+      value="2"
+      v-model="picked"
+      class="Radio-Input"
+    /><span class="Radio-Text">すっきり</span>
+    <input
+      type="radio"
+      name="q1"
+      value="1"
+      v-model="picked"
+      class="Radio-Input"
+    /><span class="Radio-Text">濃厚</span>
     <p>どんなアイスを食べてみたい？？</p>
     <input
       type="radio"
       name="q2"
       value="20"
       v-model="wasted"
-    />定番！これは食べないと！安定のアイス
+      class="Radio-Input"
+    /><span class="Radio-Text">定番！みんなが好きな安定の味</span>
     <input
       type="radio"
       name="q2"
       value="10"
       v-model="wasted"
-    />変わり種！ご当地限定おもしろアイス！
+      class="Radio-Input"
+    /><span class="Radio-Text">変わり種！ご当地限定のアイス！</span>
     <input
       type="radio"
       name="q2"
       value="1"
       v-model="wasted"
-    />贅沢なおいしさ...オトナなあなたに食べてほしい至極のアイス
+      class="Radio-Input"
+    /><span class="Radio-Text"
+      >贅沢なおいしさ...オトナなあなたに食べてほしいアイス</span
+    >
   </form>
   <button v-on:click="hyouzi">あなたにぴったりなアイスをチェック！</button>
   <div>{{ text }}</div>
@@ -66,4 +90,54 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+/*初期設定*/
+* {
+  box-sizing: border-box;
+}
+* {
+  box-sizing: border-box;
+}
+* {
+  box-sizing: border-box;
+}
+/*ラジオボタンを消す*/
+.Radio-Input {
+  appearance: none;
+  position: absolute;
+}
+/*新しくボタンを作る*/
+.Radio-Text::before {
+  content: "";
+  display: block;
+  border-radius: 50%;
+  border: 1px solid orange;
+  width: 16px;
+  height: 16px;
+}
+.Radio-Text::after {
+  content: "";
+  position: absolute;
+  left: 4px;
+  display: block;
+  border-radius: 50%;
+  width: 10px;
+  height: 10px;
+  background-color: orange;
+}
+.Radio-Text {
+  position: relative;
+  display: flex;
+  align-items: center;
+}
+.Radio-Input:checked + .Radio-Text::after {
+  content: "";
+  position: absolute;
+  left: calc(8px - 5px);
+  display: block;
+  border-radius: 50%;
+  width: 10px;
+  height: 10px;
+  background-color: orange;
+}
+</style>
