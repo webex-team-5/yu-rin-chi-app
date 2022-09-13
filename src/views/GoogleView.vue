3084cf64eb9b694dde3a4740cb1a8ab34e7220d4
Shun153 <shunyegongqi@gmail.com>
2022-09-14 00:03:53 +0900

    ピン立て成功

--- src/views/GoogleView.vue
+++ src/views/GoogleView.vue
@@ -9,24 +9,17 @@ export default {
   name: "App",
   data() {
     return {
-      myLatLing: { lat: -34.397, lng: 150.644},
-    };
+      map: undefined,
+    }
   },
   mounted() {
-    let timer = setInterval(() => {
-      if (window.google) {
-        clearInterval(timer);
     const map = new this.$gm.Map(document.getElementById("map"), {
       center: { lat: 34.855273888888888, lng: 135.30649 }, //自由な緯度・経度を入力
       zoom: 10,
     })
-    new window.google.maps.Marker({
-          position: this.myLatLng,
-          map,
-        });
-  } 500)
- },
-};
+    this.map = map
+  },
+}
 </script>
 <style scoped>
 .map {
