5bf15c4bbe1594bb4eea6d519add3d960b83e892
syoe <shoe657sunflower@yahoo.co.jp>
2022-09-22 00:01:37 +0900

    アイスが動きます

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,49 @@
+＜自動スクロール＞
 <template>
   <div class="home">Let's search icecream!</div>
-  <div class="comment"></div>
+  <Carousel :autoplay="2000" :wrap-around="true">
+    <Slide v-for="slide in 10" :key="slide">
+      <div class="carousel__item">
+        <slide> <img src="../assets/ice_cornet.png" /> </slide>
+      </div>
+      <div class="carousel__item">
+        <slide> <img src="../assets/rabenda.png" /> </slide>
+      </div>
+      <div class="carousel__item">
+        <slide> <img src="../assets/renkon.png" /> </slide>
+      </div>
+      <div class="carousel__item">
+        <slide> <img src="../assets/tyu-rip.png" /> </slide>
+      </div>
+      <div class="carousel__item">
+        <slide> <img src="../assets/wasabi.png" /> </slide>
+      </div>
+      <div class="carousel__item">
+        <slide> <img src="../assets/tadatyamame.png" /> </slide>
+      </div>
+    </Slide>
+
+    <template #addons>
+      <navigation />
+      <pagination />
+    </template>
+  </Carousel>
 </template>
 
-<script></script>
+<script>
+import "vue3-carousel/dist/carousel.css"
+import { Carousel, Slide, Pagination, Navigation } from "vue3-carousel"
+
+export default {
+  name: "App",
+  components: {
+    Carousel,
+    Slide,
+    Pagination,
+    Navigation,
+  },
+}
+</script>
 
 <style>
 .home {
@@ -15,6 +55,9 @@
   padding-bottom: 17%;
   color: white;
 }
-.comment {
+.carousel__slide {
+  height: 500px;
+  display: flex;
+  justify-content: center;
 }
 </style>
